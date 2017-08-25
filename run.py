1663a5f2a18705cabb1b0d35229f54ad6d614b2d
Haley Owen <haleyowen@utexas.edu>
2017-08-25 15:53:15 -0400

    adding port and host to run

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 
@@ -11,4 +12,5 @@ def hello_monkey():
     return str(resp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
