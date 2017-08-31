e8057f9088e069a20c891f939ee0503b0fccb124
Haley Owen <haleyowen@utexas.edu>
2017-08-31 14:55:38 -0400

    testing things

--- run.py
+++ run.py
@@ -1,15 +1,30 @@
 import os
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse, Message
+from twilio.rest import Client
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
+def response():
 
-    resp = MessagingResponse().message("Hello, Mobile Monkey")
-    return '<?xml version="1.0" encoding="UTF-8"?><Response><Message>Hello, Mobile Monkey</Message></Response>'
+    body = request.values.get('Body', None)
+    print body
+
+    send_message_to_gps()
+    return get_response("Hello, Mobile Monkey")
+
+def get_response(message):
+    return '<?xml version="1.0" encoding="UTF-8"?><Response><Message>' + message + '</Message></Response>'
+    
+def send_message_to_gps():
+    account_sid = ""
+    auth_token = ""
+
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(to="", from_="", body="GPS#")
+    print 'message: ' + str(message)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
